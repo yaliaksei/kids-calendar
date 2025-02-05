9bca4c3c7ca411b6d79805ab1d370704befc7943
Aliaksei Sery <aliaksei.sery@gmail.com>
2025-02-05 16:52:14 -0500

    add simple delete logic

--- resources/delete_lambda/delete_lambda.py
+++ resources/delete_lambda/delete_lambda.py
@@ -1,15 +1,18 @@
 # create AWS labmda with lambda_handler as handler
 import boto3
-import json
 import logging
+import os
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 def lambda_handler(event, context):
-    logger.info('got event{}'.format(event))
-    logger.info('context{}'.format(context))
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Hello from Delete Lambda!')
-    }
+    # delete calendar file from S3 bucket
+    bucket_name = os.environ['BUCKET_NAME']
+    file_name = "district_calendar.ics"
+    s3 = boto3.resource('s3')
+    
+    obj = s3.Object(bucket_name, file_name)
+    obj.delete()
+    logger.info('deleted calendar file from S3 bucket')
+
